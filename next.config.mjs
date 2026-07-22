b567cd9434445f3c3e196dd7018b2ceb905e4138
spsnft <87615686+spsnft@users.noreply.github.com>
2026-07-22 23:19:15 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Указываем базовый путь для размещения на tsvetkov.site/420
+  basePath: '/420',
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   // Оптимизация CSS (убирает блокирующие ресурсы)
@@ -14,7 +16,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com', // Добавь это, раз мы используем Cloudinary для лого
+        hostname: 'res.cloudinary.com',
       },
     ],
   },
