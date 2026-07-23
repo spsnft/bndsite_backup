d41accc725e0d57997b388ad7c3638260b22a0fe
spsnft <87615686+spsnft@users.noreply.github.com>
2026-07-23 15:37:33 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { getProducts } from "@/lib/product"
 import LandingClient from "@/components/LandingClient"
 
-// Ставим 0, чтобы данные обновлялись мгновенно при каждой загрузке (без кеша)
-// Если захочешь вернуть кеш позже — поставь 60 или более.
+// Данные обновляются каждые 60 секунд (можно менять)
 export const revalidate = 60;
 
 export default async function Page() {
-  // Запрос выполняется на сервере
-  // getProducts() возвращает весь объект из скрипта (products, stories, descriptions)
+  // Загружаем товары из Google Таблицы через API
   const data = await getProducts();
   
   return (
