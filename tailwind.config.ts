8c468313fd2b63a2e7f400dd616dfaa6196741fc
spsnft <87615686+spsnft@users.noreply.github.com>
2026-07-27 01:31:22 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-montserrat)', 'system-ui', 'sans-serif'],
-        display: ['var(--font-heading)', 'var(--font-montserrat)', 'system-ui', 'sans-serif'],
       },
       borderRadius: {
         'button': '1rem',
