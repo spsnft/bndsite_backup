75264a36011bf850f84d67a0c6d3f2bace038fc8
spsnft <87615686+spsnft@users.noreply.github.com>
2026-07-27 00:43:06 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import { getProducts } from "@/lib/product"
 export const revalidate = 60;
 
 export default async function Page() {
-  const { products, descriptions } = await getProducts();
+  const { products, descriptions, categories } = await getProducts();
 
   return (
     <main>
       <LandingClient 
         initialProducts={products} 
-        initialDescriptions={descriptions} 
+        initialDescriptions={descriptions}
+        categories={categories}
       />
     </main>
   );
