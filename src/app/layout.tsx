b05a2f4424534ae8ff6dc2b7422ded3027227571
spsnft <87615686+spsnft@users.noreply.github.com>
2026-07-27 16:11:37 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,6 @@ export const viewport: Viewport = {
   themeColor: siteConfig.themeColor,
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 }
 
 export const metadata: Metadata = {
